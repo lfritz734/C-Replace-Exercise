d0722b6318313b6fb61835df38c9e648e5c1374f
lfritz734 <129979010+lfritz734@users.noreply.github.com>
2023-04-05 12:26:21 -0400

    Add files via upload

---
+++ Exercise 7-14-2021/Exercise 7-14-2021/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Exercise_7_14_2021
+{
+    class Program
+    {
+        static void Main()
+        {
+            Encoding();
+        }
+        private static void Encoding()
+        {
+            string userInput;
+            Write("Enter a phrase: ");  //request user input
+            userInput = ReadLine();
+            var replacement = ("");
+            for (int i = 0; i < userInput.Length; ++i)  //keeps loop within length of input
+            {
+                replacement = userInput.Replace("a", "4").Replace("e", "3").Replace("i", "1");
+            }
+            WriteLine(replacement);
+        }
+    }
+}
